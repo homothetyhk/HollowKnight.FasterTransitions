5b64376aefb3ec7e3a1b4b5b263d9d3a55d65434
homothetyhk@gmail.com <homothetyhk@gmail.com>
2020-04-16 16:04:34 -0400

    Add project files.

---
+++ HollowKnight.FasterTransitions/FasterTransitions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Modding;
+using MonoMod;
+using MonoMod.RuntimeDetour;
+using MonoMod.Cil;
+using UnityEngine;
+using Mono.Cecil.Cil;
+
+namespace FasterTransitions
+{
+    public class FasterTransitions : Mod, ITogglableMod
+    {
+        public static ILHook fasterTransitionHook;
+
+        public override void Initialize()
+        {
+            Type T = typeof(HeroController).GetNestedTypes(BindingFlags.NonPublic | BindingFlags.Instance).First(t => t.Name == "<EnterScene>c__Iterator0");
+            MethodBase M = T.GetMethod("MoveNext", BindingFlags.Instance | BindingFlags.Public);
+            fasterTransitionHook = new ILHook(M, RemoveTransitionWaits);
+        }
+
+        private void RemoveTransitionWaits(MonoMod.Cil.ILContext il)
+        {
+            ILCursor c = new ILCursor(il).Goto(0);
+            while (c.TryGotoNext(
+                i => i.OpCode == OpCodes.Ldc_R4,
+                i => i.OpCode == OpCodes.Newobj && i.MatchNewobj<WaitForSeconds>()
+                ))
+            {
+                if ((float)c.Instrs[c.Index].Operand == .4f)
+                {
+                    c.Remove();
+                    c.Emit(OpCodes.Ldc_R4, 0f);
+                    c.Index += 2;
+                }
+                else if ((float)c.Instrs[c.Index].Operand == .165f)
+                {
+                    c.Remove();
+                    c.Emit(OpCodes.Ldc_R4, 0f);
+                    c.Index += 2;
+                }
+                else if ((float)c.Instrs[c.Index].Operand == .25f || (float)c.Instrs[c.Index].Operand == .33f)
+                {
+                    c.Index += 2;
+                }
+            }
+        }
+
+        public override string GetVersion()
+        {
+            return "1.0";
+        }
+
+        public void Unload()
+        {
+            fasterTransitionHook.Dispose();
+        }
+    }
+}
